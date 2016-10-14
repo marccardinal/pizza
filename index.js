3840977e323300744f04e02a0abe345313dfeafe
Marc Vieira-Cardinal <marc.cardinal@gmail.com>
2016-10-14 09:14:14 -0400

    Set a default value for the workers argument

--- index.js
+++ index.js
@@ -6,9 +6,22 @@ var program = require('commander');
 
 var Simulation = require('./libs/simulation.js');
 
+
+// Related to https://github.com/tj/commander.js/issues/201
+function argInt(string, defaultValue) {
+  var int = parseInt(string, 10);
+
+  if (typeof int == 'number') {
+    return int;
+  } else {
+    return defaultValue;
+  }
+} 
+
+
 program
   .usage('[options] <file>')
-  .option('-w, --workers <n>', 'Number of delivery people')
+  .option('-w, --workers <n>', 'Number of delivery people', argInt, 1)
   .option('-p, --pretty', 'Display a pretty version of the resulsts')
   .action(function(file) {
     console.log("Running with %s and %s workers", file, program.workers);
